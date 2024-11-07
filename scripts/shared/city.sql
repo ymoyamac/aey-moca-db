35e841365aa189b4b190eb01976ea7e1c794ca1e
ymoyamac <yael.moya.mac@outlook.com>
2024-11-06 21:02:30 -0600

    feat: Update queries insertions

--- scripts/shared/city.sql
+++ scripts/shared/city.sql
@@ -14,21 +14,21 @@ CREATE TABLE IF NOT EXISTS public.T_SHR03_CITIES(
 --
 
 ALTER TABLE public.T_SHR03_CITIES
-	ADD CONSTRAINT fk01_T_SHR03_CITIES_country
+	ADD CONSTRAINT fk01_T_SHR03_CITIES_TO_COUNTRY
 		FOREIGN KEY(cty_fk_country_id)
 		REFERENCES T_SHR02_COUNTRIES(country_id);
 
 --
 
-INSERT INTO T_SHR03_CITIES VALUES (1, 'Ciudad de México', 'CDMX', NOW(), NOW() true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (2, 'México', 'MX', NOW(), NOW() true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (3, 'Querétaro', 'QT', NOW(), NOW() true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (4, 'Buenos aires', 'LM', NOW(), NOW() true, 8);
-INSERT INTO T_SHR03_CITIES VALUES (5, 'Barcelona', 'BC', NOW(), NOW() true, 3);
-INSERT INTO T_SHR03_CITIES VALUES (6, 'San Salvador', 'SS', NOW(), NOW() true, 6);
-INSERT INTO T_SHR03_CITIES VALUES (7, 'Santiago de Chile', 'SCHL', NOW(), NOW() true, 4);
-INSERT INTO T_SHR03_CITIES VALUES (8, 'Lima', 'LI', NOW(), NOW() true, 4);
-INSERT INTO T_SHR03_CITIES VALUES (9, 'Madrid', 'LI', NOW(), NOW() true, 4);
+INSERT INTO T_SHR03_CITIES VALUES (1, 'Ciudad de México', 'CDMX', NOW(), NOW(), true, 1);
+INSERT INTO T_SHR03_CITIES VALUES (2, 'México', 'MX', NOW(), NOW(), true, 1);
+INSERT INTO T_SHR03_CITIES VALUES (3, 'Querétaro', 'QT', NOW(), NOW(), true, 1);
+INSERT INTO T_SHR03_CITIES VALUES (4, 'Buenos aires', 'LM', NOW(), NOW(), true, 8);
+INSERT INTO T_SHR03_CITIES VALUES (5, 'Barcelona', 'BC', NOW(), NOW(), true, 3);
+INSERT INTO T_SHR03_CITIES VALUES (6, 'San Salvador', 'SS', NOW(), NOW(), true, 6);
+INSERT INTO T_SHR03_CITIES VALUES (7, 'Santiago de Chile', 'SCHL', NOW(), NOW(), true, 4);
+INSERT INTO T_SHR03_CITIES VALUES (8, 'Lima', 'LI', NOW(), NOW(), true, 4);
+INSERT INTO T_SHR03_CITIES VALUES (9, 'Madrid', 'LI', NOW(), NOW(), true, 4);
 
 --
 
@@ -38,4 +38,4 @@ SELECT city_id AS id,
     cty_dt_created_at AS created_at,
     cty_dt_updated_at AS updated_at,
     cty_st_is_active AS is_active
-FROM shr02_countries;
+FROM T_SHR03_CITIES;
--- scripts/shared/country.sql
+++ scripts/shared/country.sql
@@ -14,15 +14,15 @@ CREATE TABLE IF NOT EXISTS public.T_SHR02_COUNTRIES(
 
 --
 
-INSERT INTO T_SHR02_COUNTRIES VALUES (1, 'México', 'MX', 484, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (2, 'Canada', 'CAN', 124, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (3, 'España', 'ESP', 724, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (4, 'Chile', 'CHL', 152, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (5, 'Colombia', 'CO', 170, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (6, 'El Salvador', 'SV', 222, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (7, 'Panama', 'PAN', 591, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (8, 'Argentina', 'ARG', 32, now(), now(), true);
-INSERT INTO T_SHR02_COUNTRIES VALUES (9, 'Perú', 'PR', 604, now(), now(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (1, 'México', 'MX', 484, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (2, 'Canada', 'CAN', 124, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (3, 'España', 'ESP', 724, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (4, 'Chile', 'CHL', 152, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (5, 'Colombia', 'CO', 170, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (6, 'El Salvador', 'SV', 222, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (7, 'Panama', 'PAN', 591, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (8, 'Argentina', 'ARG', 32, NOW(), NOW(), true);
+INSERT INTO T_SHR02_COUNTRIES VALUES (9, 'Perú', 'PR', 604, NOW(), NOW(), true);
 
 --
 
