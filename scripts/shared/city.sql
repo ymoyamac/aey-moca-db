932b32d9e80a7330d2a5c788589f477a93d89761
ymoyamac <yael.moya.mac@outlook.com>
2024-11-09 19:30:28 -0600

    feat: Add permissions entity

--- scripts/shared/city.sql
+++ scripts/shared/city.sql
@@ -1,6 +1,7 @@
---T_SHR03_CITIES
-
-CREATE TABLE IF NOT EXISTS public.T_SHR03_CITIES(
+-- --------------------------------------------
+-- T_CITIES
+-- --------------------------------------------
+CREATE TABLE IF NOT EXISTS public.T_CITIES(
 	city_id              	INTEGER UNIQUE NOT NULL,
 	cty_tx_name        		VARCHAR(50) NOT NULL,
 	cty_tx_abbrevation  	VARCHAR(10) NOT NULL,
@@ -13,22 +14,22 @@ CREATE TABLE IF NOT EXISTS public.T_SHR03_CITIES(
 
 --
 
-ALTER TABLE public.T_SHR03_CITIES
-	ADD CONSTRAINT fk01_T_SHR03_CITIES_TO_COUNTRY
+ALTER TABLE public.T_CITIES
+	ADD CONSTRAINT fk01_T_CITIES_TO_COUNTRY
 		FOREIGN KEY(cty_fk_country_id)
 		REFERENCES T_SHR02_COUNTRIES(country_id);
 
 --
 
-INSERT INTO T_SHR03_CITIES VALUES (1, 'Ciudad de México', 'CDMX', NOW(), NOW(), true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (2, 'México', 'MX', NOW(), NOW(), true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (3, 'Querétaro', 'QT', NOW(), NOW(), true, 1);
-INSERT INTO T_SHR03_CITIES VALUES (4, 'Buenos aires', 'LM', NOW(), NOW(), true, 8);
-INSERT INTO T_SHR03_CITIES VALUES (5, 'Barcelona', 'BC', NOW(), NOW(), true, 3);
-INSERT INTO T_SHR03_CITIES VALUES (6, 'San Salvador', 'SS', NOW(), NOW(), true, 6);
-INSERT INTO T_SHR03_CITIES VALUES (7, 'Santiago de Chile', 'SCHL', NOW(), NOW(), true, 4);
-INSERT INTO T_SHR03_CITIES VALUES (8, 'Lima', 'LI', NOW(), NOW(), true, 4);
-INSERT INTO T_SHR03_CITIES VALUES (9, 'Madrid', 'LI', NOW(), NOW(), true, 4);
+INSERT INTO T_CITIES VALUES (1, 'Ciudad de México', 'CDMX', NOW(), NOW(), true, 1);
+INSERT INTO T_CITIES VALUES (2, 'México', 'MX', NOW(), NOW(), true, 1);
+INSERT INTO T_CITIES VALUES (3, 'Querétaro', 'QT', NOW(), NOW(), true, 1);
+INSERT INTO T_CITIES VALUES (4, 'Buenos aires', 'LM', NOW(), NOW(), true, 8);
+INSERT INTO T_CITIES VALUES (5, 'Barcelona', 'BC', NOW(), NOW(), true, 3);
+INSERT INTO T_CITIES VALUES (6, 'San Salvador', 'SS', NOW(), NOW(), true, 6);
+INSERT INTO T_CITIES VALUES (7, 'Santiago de Chile', 'SCHL', NOW(), NOW(), true, 4);
+INSERT INTO T_CITIES VALUES (8, 'Lima', 'LI', NOW(), NOW(), true, 4);
+INSERT INTO T_CITIES VALUES (9, 'Madrid', 'LI', NOW(), NOW(), true, 4);
 
 --
 
@@ -38,4 +39,4 @@ SELECT city_id AS id,
     cty_dt_created_at AS created_at,
     cty_dt_updated_at AS updated_at,
     cty_st_is_active AS is_active
-FROM T_SHR03_CITIES;
+FROM T_CITIES;
--- scripts/shared/gender.sql
+++ scripts/shared/gender.sql
@@ -1,6 +1,8 @@
---T_SHR01_GENDERS
+-- --------------------------------------------
+-- T_GENDERS
+-- --------------------------------------------
 
-CREATE TABLE IF NOT EXISTS public.T_SHR01_GENDERS(
+CREATE TABLE IF NOT EXISTS public.T_GENDERS(
 	gender_id           	INTEGER UNIQUE NOT NULL,
 	gen_tx_name         	VARCHAR(20) UNIQUE NOT NULL,
 	gen_tx_abbrevation  	VARCHAR(2) UNIQUE NOT NULL,
@@ -12,9 +14,9 @@ CREATE TABLE IF NOT EXISTS public.T_SHR01_GENDERS(
 
 --
 
-INSERT INTO T_SHR01_GENDERS VALUES (1, 'MALE', 'M', NOW(), NOW(), TRUE);
-INSERT INTO T_SHR01_GENDERS VALUES (2, 'FEMALE', 'F', NOW(), NOW(), TRUE);
-INSERT INTO T_SHR01_GENDERS VALUES (2, 'OTHER', 'O', NOW(), NOW(), TRUE);
+INSERT INTO T_GENDERS VALUES (1, 'MALE', 'M', NOW(), NOW(), TRUE);
+INSERT INTO T_GENDERS VALUES (2, 'FEMALE', 'F', NOW(), NOW(), TRUE);
+INSERT INTO T_GENDERS VALUES (2, 'OTHER', 'O', NOW(), NOW(), TRUE);
 
 --
 
@@ -24,4 +26,4 @@ SELECT gender_id AS id,
     gen_dt_created_at AS created_at,
     gen_dt_updated_at AS updated_at,
     gen_st_is_active AS is_active
-FROM T_SHR01_GENDERS;
+FROM T_GENDERS;
